11168d56e89a186c01fb41905b4dfdef961f39b0
Ephrem Ketema <e_ketema@github.com>
2019-02-05 18:59:25 -0500

    dasdsa

--- src/test/java/utilities/BrowserUtilities.java
+++ src/test/java/utilities/BrowserUtilities.java
@@ -20,6 +20,17 @@ import java.util.Random;
 public class BrowserUtilities {
 
 
+    public static void wait(int second){
+
+        try {
+            Thread.sleep(1000*second);
+        }catch (Exception e){
+
+        }
+
+    }
+
+
     public static boolean isClickable(WebElement element) {
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
         try {
